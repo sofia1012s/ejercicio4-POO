fd7e0bed3b60a9858e11e176c9ecd6bfc3480ffe
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-09-28 18:43:04 -0600

    Update Combatiente.java

--- Combatiente.java
+++ Combatiente.java
@@ -1,3 +1,11 @@
+/******************************************************************************
+ * Combatiente.java
+ * 
+ * @author Sofía Salguero
+ * @version 28/09/2021 Clase Combatiente, se encarga de tener los métodos
+ *          generales para los combatientes, para obtener sus propios valores y
+ *          utilizarlos por aparte.
+ ******************************************************************************/
 public class Combatiente {
     protected String nombre;
     protected int puntosVida;
@@ -6,6 +14,14 @@ public class Combatiente {
     protected boolean tomarTurno;
     protected int items;
 
+    /**
+     * 
+     * @param nombre
+     * @param puntosVida
+     * @param poderAtaque
+     * @param turno
+     * @param items
+     */
     public Combatiente(String nombre, int puntosVida, int poderAtaque, int turno, int items) {
         this.nombre = nombre;
         this.puntosVida = puntosVida;
@@ -14,23 +30,38 @@ public class Combatiente {
         this.items = items;
     }
 
+    /**
+     * @return boolean
+     */
     public boolean tomarTurno() {
         tomarTurno = true;
         return tomarTurno;
     }
 
+    /**
+     * @return String
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * @return int
+     */
     public int getPuntosVida() {
         return puntosVida;
     }
 
+    /**
+     * @return int
+     */
     public int getPoderAtaque() {
         return poderAtaque;
     }
 
+    /**
+     * @return int
+     */
     public int getTurno() {
         return turno;
     }
