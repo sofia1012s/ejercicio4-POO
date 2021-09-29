3b48ebde561d33c0ed7090b5610feb57977b8579
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-09-28 18:42:37 -0600

    Update Bruja.java

--- Bruja.java
+++ Bruja.java
@@ -1,8 +1,23 @@
-public class Bruja extends Enemigo{
+/******************************************************************************
+ * Bruja.java
+ * 
+ * @author Sofía Salguero
+ * @version 28/09/2021 Clase Bruja, se encarga de mantener las propiedades de un
+ *          enemigo de tipo bruja
+ ******************************************************************************/
+public class Bruja extends Enemigo {
+    /**
+     * 
+     * @param nombre
+     * @param puntosVida
+     * @param poderAtaque
+     * @param turno
+     * @param items
+     */
 
     public Bruja(String nombre, int puntosVida, int poderAtaque, int turno, int items) {
         super(nombre, puntosVida, poderAtaque, turno, items);
-        //TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub
     }
-    
+
 }
