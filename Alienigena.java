fab6df85c990bcace437cb881b7cbc781b3bd074
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-09-28 18:42:00 -0600

    Update Alienigena.java

--- Alienigena.java
+++ Alienigena.java
@@ -1,8 +1,22 @@
-public class Alienigena extends Enemigo{
-
+/******************************************************************************
+ * Alienigena.java
+ * 
+ * @author Sofía Salguero
+ * @version 28/09/2021 Clase Alienigena, se encarga de mantener las propiedades
+ *          de un enemigo de tipo alienigena
+ ******************************************************************************/
+public class Alienigena extends Enemigo {
+    /**
+     * 
+     * @param nombre
+     * @param puntosVida
+     * @param poderAtaque
+     * @param turno
+     * @param items
+     */
     public Alienigena(String nombre, int puntosVida, int poderAtaque, int turno, int items) {
         super(nombre, puntosVida, poderAtaque, turno, items);
-        //TODO Auto-generated constructor stub
+        // TODO Auto-generated constructor stub
     }
-    
+
 }
