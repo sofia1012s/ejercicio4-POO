8f421cc7e0d74b7421340ce4455a1fedc149adf9
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-09-28 18:43:24 -0600

    Update Explorador.java

--- Explorador.java
+++ Explorador.java
@@ -1,14 +1,37 @@
-public class Explorador extends Jugador {
+/******************************************************************************
+ * Explorador.java
+ * 
+ * @author Sofía Salguero
+ * @version 28/09/2021 Clase Explorador, se encarga de mantener las propiedades
+ *          de un jugador de tipo explorador
+ ******************************************************************************/
 
+ public class Explorador extends Jugador {
+     /**
+      * 
+      * @param nombre
+      * @param puntosVida
+      * @param poderAtaque
+      * @param turno
+      * @param items
+      */
     public Explorador(String nombre, int puntosVida, int poderAtaque, int turno,int items) {
         super(nombre, puntosVida, poderAtaque, turno, items);
     }
 
+    
+    /** 
+     * @return int
+     */
     public int setPuntosVida() {
         this.puntosVida = 200;
         return puntosVida;
     }
 
+    
+    /** 
+     * @return int
+     */
     public int setPoderAtaque() {
         this.poderAtaque = 10;
         return poderAtaque;
