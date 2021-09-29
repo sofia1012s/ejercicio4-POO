82832b3db5d9c31078025ee967ea66635ae234c3
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-09-28 18:43:36 -0600

    Update Guerrero.java

--- Guerrero.java
+++ Guerrero.java
@@ -1,5 +1,20 @@
+/******************************************************************************
+ * Guerrero.java
+ * 
+ * @author Sofía Salguero
+ * @version 28/09/2021 
+ * Clase Guerrero, se encarga de mantener los parámetros propios de un jugador
+ * de tipo guerrero
+ ******************************************************************************/
 public class Guerrero extends Jugador {
-
+    /**
+     * 
+     * @param nombre
+     * @param puntosVida
+     * @param poderAtaque
+     * @param turno
+     * @param items
+     */
     public Guerrero(String nombre, int puntosVida, int poderAtaque, int turno, int items) {
         super(nombre, puntosVida, poderAtaque, turno, items);
         // TODO Auto-generated constructor stub
